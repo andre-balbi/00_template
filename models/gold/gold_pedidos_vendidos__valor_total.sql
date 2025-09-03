50cb220f8c23c8179a2e8787c14cf12308d56688
drebalbi <nenhum@nenhum.com>
2025-09-03 13:15:37 -0300

    first commit

--- models/gold/gold_pedidos_vendidos__valor_total.sql
+++ models/gold/gold_pedidos_vendidos__valor_total.sql
@@ -11,6 +11,8 @@ select
     , count(*) as total_pedidos_vendidos
     , sum(valor) as valor_total_vendido
     , avg(valor) as valor_medio_pedido
+    , min(valor) as valor_minimo_pedido
+    , max(valor) as valor_maximo_pedido
 from {{ ref('silver_pedidos_vendidos') }}
 group by
     data_pedido
